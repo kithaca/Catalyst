a9fb5cc53e1310a4c41502eea3d5cf44ed2a3bcf
App Academy Student <kithaca@gmail.com>
2016-03-07 01:24:19 -0800

    some cosmetic changes to project display

--- frontend/components/projects/projectIndex.jsx
+++ frontend/components/projects/projectIndex.jsx
@@ -28,6 +28,17 @@ var ProjectIndex = React.createClass({
     this.projectListener.remove();
   },
 
+  shuffle: function (arr) {
+    for (var i = arr.length-1; i >= 0; i--) {
+      var randIdx = Math.floor(Math.random()*(i+1));
+      var currEntry = arr[randIdx];
+
+      arr[randIdx] = arr[i];
+      arr[i] = currEntry;
+    }
+   return arr;
+  },
+
   render: function () {
     this.state.projects.forEach(function (project) {
       if (project.image_url === null) {
@@ -35,12 +46,19 @@ var ProjectIndex = React.createClass({
       }
     });
 
+    var projectsArray = [];
+    var that = this;
+    Object.keys(that.state.projects).forEach(function (key) {
+      projectsArray.push(that.state.projects[key]);
+    });
+    projectsArray = this.shuffle(projectsArray);
+
     return(
       <div>
         <ProjectCarousel />
         <div className="project-index group">
         <ul>
-          {this.state.projects.map(function (project) {
+          {projectsArray.map(function (project) {
             return (<ProjectIndexItem key={project.id} project={project} />);
           })}
         </ul>
--- frontend/components/projects/projectIndexItem.jsx
+++ frontend/components/projects/projectIndexItem.jsx
@@ -23,12 +23,12 @@ var ProjectIndexItem = React.createClass({
                    alt="project_image" />
         <h4 className="proj-category">{this.props.project.category}</h4>
         <h5 className="proj-title">{this.props.project.title}</h5>
-        <h6 className="proj-creator">Creator: {this.props.project.creator}</h6>
         <h6 className="proj-tagline">{this.props.project.tagline}</h6>
-        <p className="proj-descr">{this.props.project.description}</p>
+        <h6 className="proj-creator">Creator: {this.props.project.creator}</h6>
         <h4 className="proj-goal">Funding Goal: ${this.props.project.goal_amt}</h4>
         <h6 className="proj-start">Start: {this.props.project.start_date}</h6>
         <h6 className="proj-deadline">Deadline: {this.props.project.deadline}</h6>
+        <p className="proj-descr">{this.props.project.description}</p>
       </li>
     );
   }
